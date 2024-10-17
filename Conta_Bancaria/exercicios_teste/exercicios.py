d70c5ce3382441b33bdc4b3e95e03785dee5cce7
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-10-17 16:44:27 -0300

    Add files via upload

---
+++ Conta_Bancaria/exercicios_teste/exercicios.py
@@ -0,0 +1,22 @@
+'''
+Faça um algoritmo para ler dois números e imprimir a soma dos números lidos.
+
+'''
+x = int(input('Digite um valor: '))
+y = int(input('Digite outro valor: '))
+
+print('Primeiro número: ', x)
+print('Segundo número: ', y)
+print('soma dos valores = ',x + y)
+
+
+'''
+
+Faça um algoritmo para ler três números e imprimir a soma, média e produto dos números lidos.
+
+'''
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+valor3 = int(input('Digite o terceiro valor: '))
+
+print()
