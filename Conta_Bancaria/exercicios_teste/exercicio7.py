92d8bc7a9eeb4f5cff22338fd2348315cde606e1
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-10-18 16:32:35 -0300

    Add files via upload

---
+++ Conta_Bancaria/exercicios_teste/exercicio7.py
@@ -0,0 +1,19 @@
+'''
+Faça um algoritmo para ler três números e imprimir o maior.
+'''
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+if n1 > n2 and n3:
+    print(f'O maior é: {n1}')
+
+if n2 > n1 and n3:
+    print(f'O maior é: {n2}')
+
+if n3 > n1 and n2:
+    print(f'O maior é: {n3}')
+
+elif n1 == n2 == n3:
+    print("Os números são iguais!")
