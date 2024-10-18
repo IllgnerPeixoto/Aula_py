eccd8462260926c8185dac55a457e27a2dc9813d
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-10-18 14:55:33 -0300

    Add files via upload

---
+++ Conta_Bancaria/exercicios_teste/exercicio3.py
@@ -0,0 +1,16 @@
+'''
+Faça um algoritmo para ler dois números e imprimir o maior, o menor ou então dizer se são iguais.
+
+'''
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+
+if n1 > n2:
+    print(f'O valor {n1} é maior que {n2}')
+
+if n1 < n2: 
+    print(f'O valor {n2} é maior que {n1}')
+
+else:
+    print(f'Os valores são iguais')
