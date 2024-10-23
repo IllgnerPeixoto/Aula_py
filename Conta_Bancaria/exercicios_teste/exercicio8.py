5583a2b387bbbab906f592a9dd90d6d1d157857b
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-10-23 16:59:01 -0300

    Add files via upload

---
+++ Conta_Bancaria/exercicios_teste/exercicio8.py
@@ -0,0 +1,14 @@
+'''
+Faça um algoritmo para ler três números e imprimir se estes podem ou não formar um triângulo.
+Observação - Para formar os lados de um triângulo cada um dos valores tem que ser menor que a soma dos outros dois.
+'''
+
+#Leia os lados dos triângulos
+
+print('Entre com os 3 números para os lados do triângulo: ')
+l1 = int(input('Lado 1: '))
+l2 = int(input('Lado 2: '))
+l3 = int(input('Lado 3: '))
+
+print(f'Você digitou os números: {l1}, {l2}, {l3}')
+
