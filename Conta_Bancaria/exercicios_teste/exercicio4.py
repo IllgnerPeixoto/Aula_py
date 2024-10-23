0abe1f9537602784fcd6fef176229837d699eec7
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-10-23 15:02:49 -0300

    exercicio4.py

--- Conta_Bancaria/exercicios_teste/exercicio4.py
+++ Conta_Bancaria/exercicios_teste/exercicio4.py
@@ -2,10 +2,11 @@
 Faça um algoritmo para ler um número inteiro e dizer se o número lido é par ou impar.
 '''
 
-n = int(input("Digite um número: "))
-
-if n % 2 == 0:
-    print(f"Número {n} é par.")
-
-else: 
-    print(f'Número {n} é impar.')
+while True:
+    n = int(input("Digite um número: "))
+    if n % 2 == 0:
+        print(f"Número {n} é par.")
+    else: 
+        print(f'Número {n} é impar.')
+    continuar = input('Deseja continuar? (Digite s para sim)')
+    if continuar != 's': break
