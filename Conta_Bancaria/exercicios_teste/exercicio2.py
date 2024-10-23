03ce3074a5391796962ad33150cac76e4d981e62
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-10-23 14:26:00 -0300

    exercicio2.py

--- Conta_Bancaria/exercicios_teste/exercicio2.py
+++ Conta_Bancaria/exercicios_teste/exercicio2.py
@@ -3,10 +3,15 @@
 Faça um algoritmo para ler três números e imprimir a soma, média e produto dos números lidos.
 
 '''
-valor1 = float(input('Digite o primeiro valor: '))
-valor2 = float(input('Digite o segundo valor: '))
-valor3 = float(input('Digite o terceiro valor: '))
+n1 = float(input('Digite o primeiro valor: '))
+n2 = float(input('Digite o segundo valor: '))
+n3 = float(input('Digite o terceiro valor: '))
 
-print('Soma dos valores é: ', valor1 + valor2 + valor3)
-print('Média dos valores é: ', (valor1 + valor2 + valor3)/3)
-print('Produto dos valores é: ', valor1 * valor2 * valor3)
+soma = n1 + n2 + n3 
+print(f'Soma dos valores é: {soma}')
+
+media = soma / 3
+print(f'Média dos valores é: {media}')
+
+produto = n1 * n2 * n3
+print(f'Produto dos valores é: {produto}')
