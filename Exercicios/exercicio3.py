d312ba862cd30113d40a95658394afe7f581e70a
IllgnerPeixoto <158613207+IllgnerPeixoto@users.noreply.github.com>
2024-11-01 14:12:55 -0300

    Add files via upload

---
+++ Exercicios/exercicio3.py
@@ -0,0 +1,15 @@
+'''
+Faça um algoritmo para ler dois números e imprimir o maior, o menor ou então dizer se são iguais.
+
+'''
+while True:
+    n1 = int(input("Digite um número: "))
+    n2 = int(input("Digite outro número: "))
+    if n1 > n2:
+        print(f'O valor {n1} é maior que {n2}')
+    if n1 < n2: 
+        print(f'O valor {n2} é maior que {n1}')
+    else:
+        print(f'Os valores são iguais')
+    continuar = input('Deseja sair ? (Digite s para sair)')
+    if continuar == 's': break
